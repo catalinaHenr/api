5cc2d81a1bbd9e1a7422ad446b6e9da027c531c0
catalinaHenr <chenriquez@edudonihue.cl>
2023-05-10 17:22:58 -0400

    add

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 const pokemonContainer = document.querySelector('.pokemon-container');
 
-function fetchPokemon(id)  {
+function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
         .then((res) => res.json())
         .then((data) => {
@@ -11,12 +11,12 @@ function fetchPokemon(id)  {
 }
 
 function fetchPokemons(number) {
-    for (let i = 1; i<= number; i++) {
+    for (let i = 1; i <= number; i++) {
         fetchPokemon(i);
     }
 }
 
-function createPokemon(pokemon){
+function createPokemon(pokemon) {
     const card = document.createElement('div');
     card.classList.add('pokemon-block');
 
@@ -24,9 +24,12 @@ function createPokemon(pokemon){
     spriteContainer.classList.add('img-container');
 
     const sprite = document.createElement('img');
-    sprite.src = pokemon.sprites.back_default
+    sprite.src = pokemon.sprites.front_default
 
     spriteContainer.appendChild(sprite);
+
+    const number = document.createElement('p');
+    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
 
     const name = document.createElement('p');
     name.classList.add('name');
@@ -40,10 +43,35 @@ function createPokemon(pokemon){
     height.classList.add('height');
     height.textContent = `Altura: ${pokemon.weight}`;
 
+    const types = document.createElement('p');
+    types.classList.add('types');
+    types.textContent = 'Tipo: '
+
+    pokemon.types.forEach(typeObj => {
+        const typeSpan = document.createElement('span');
+        typeSpan.textContent = typeObj.type.name;
+        types.appendChild(typeSpan);
+    });
+
+    card.addEventListener('click', () => {
+
+        const modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        const modalContent = document.createElement('div');
+        modalContent.classList.add('modal-content');
+        modal.appendChild(modalContent);
+
+        document.body.appendChild(modal);
+
+    });
+
     card.appendChild(spriteContainer);
+    card.appendChild(number);
     card.appendChild(name);
     card.appendChild(weight);
     card.appendChild(height);
+    card.appendChild(types);
 
     pokemonContainer.appendChild(card);
 }
